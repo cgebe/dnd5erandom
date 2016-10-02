845f8a0fcae984ae4abe11124051b5da05c036f6
cgebe <gebendorfer@icewave.org>
2016-10-02 21:29:25 +0200

    merchant selects

--- src/app/merchant/merchant.ts
+++ src/app/merchant/merchant.ts
@@ -6,4 +6,21 @@ import {Component} from '@angular/core';
   templateUrl: './merchant.html'
 })
 export class Merchant {
+    citySizes = 'tiny small medium large big'.split(' ');
+    selectedCitySize = 'small';
+
+    merchantTypes = 'weapon armor trader alchemist scribe wandwright'.split(' ');
+    selectedMerchantType = 'weapon';
+
+    onChangeCitySize(newValue) {
+        console.log(newValue);
+        this.selectedCitySize = newValue;
+        // ... do other stuff here ...
+    }
+
+    onChangeMerchantType(newValue) {
+        console.log(newValue);
+        this.selectedMerchantType = newValue;
+        // ... do other stuff here ...
+    }
 }
