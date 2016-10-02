eeaf3aee3a14aefb9896163385231d6304c13d44
cgebe <gebendorfer@icewave.org>
2016-10-02 20:34:38 +0200

    merge

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import {Component} from '@angular/core';
 @Component({
   selector   : 'app',
   templateUrl: './app.html',
+  styleUrls: ['./common/libs/bootstrap/css/bootstrap.min.css'],
 })
 export class AppComponent {
 }
